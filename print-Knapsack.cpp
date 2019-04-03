cae4f7752abb44c713d452762be5a70d0497e612
clumsy48 <m007urray@gmail.com>
2019-04-04 02:11:51 +0530

    Update print-Knapsack.cpp
    comments

--- print-Knapsack.cpp
+++ print-Knapsack.cpp
@@ -30,11 +30,11 @@ void printStones(int weight[],int kw,int n){
         int x = mymap[{i,j}];
         int y = mymap[{i-1,j}];
         int z = mymap[{i,j-1}];
-        cout << "i "<< i << " " <<" j " << j << endl;
+       // cout << "i "<< i << " " <<" j " << j << endl;
      //   cout << "x " << x << " y " << y << " z " << z << endl; 
-        if(y==z && y==0){
+        if(y==z && y==0){ //base case when no up and left exists
             if(x!=0){
-                myset.insert(i);
+                myset.insert(i); // last value
 //cout << i << "\n";
             } 
             break; 
@@ -44,20 +44,20 @@ void printStones(int weight[],int kw,int n){
 //cout << i << "\n";
                 j = j-weight[i-1];
             } 
-            j--;
+            j--; // can up down or left {here , going up}
         }else if(y>z){
             if(x>y){
                  myset.insert(i);
                //  cout << i << "\n";
-                 j = j-weight[i-1];
-            }else i--;
+                 j = j-weight[i-1]; // decrease bag size and go up
+            }else i--; // go left
             
         }else if(y<z){
             if(x>z){
                 myset.insert(j);
              // cout << j << "\n"; 
-              j = j-weight[i-1];
-            }else j--;
+              j = j-weight[i-1]; // decrease bag size and decrease bag size
+            }else j--; // go up
         }
     }
 }
@@ -67,8 +67,8 @@ int main()
     int value[] = {8,4,0,8,0}; 
     int kw = 4;
     int n = sizeof(value)/sizeof(int);
-    cout << maxValue(value,weight,n,kw) << endl;
-    for(auto i=mymap.begin();i!=mymap.end();++i) cout << (*i).first.first <<" " << (*i).first.second << " " << (*i).second << endl;
+    cout << "maxValue " << maxValue(value,weight,n,kw) << endl;
+  //  for(auto i=mymap.begin();i!=mymap.end();++i) cout << (*i).first.first <<" " << (*i).first.second << " " << (*i).second << endl;
     printStones(weight,kw,n);
     for(auto i=myset.begin();i!=myset.end();++i) cout << *i << " ";
     return 0;
